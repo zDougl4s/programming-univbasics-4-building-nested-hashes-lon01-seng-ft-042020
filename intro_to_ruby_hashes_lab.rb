924443c959bf15f9677d9a548fc731ad39a8a789
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-10 02:09:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,9 +19,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-  {
-  :railroads => {}
-  }
+  
 end
 
 def monopoly_with_fourth_tier
