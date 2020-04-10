cbe121b4f819dc29f3533dd40842cdfde339907d
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-10 02:12:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -10,7 +10,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {
-  :railroads => {:pieces => {1} }
+  :railroads => {:pieces => {1}}
   }
 end
 
