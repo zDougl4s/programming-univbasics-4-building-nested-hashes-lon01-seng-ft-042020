e3f7416883f3daf1e7093a0539cf278ff496d9f9
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-10 02:33:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_third_tier
       :reading_railroad => {},
       :pennsylvania_railroad =>{},
       :b_and_o_railroad =>{},
-      :shortline_railroad =>{},
+      :shortline_railroad =>{}
     }
 
  }
